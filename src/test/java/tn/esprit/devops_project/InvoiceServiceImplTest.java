39dfd710e9ff0d6312a9f1d8abdc104831b8be91
HaithemGhattass <haithem.ghattas@esprit.tn>
2023-10-26 20:05:33 +0100

    adding some test

---
+++ src/test/java/tn/esprit/devops_project/InvoiceServiceImplTest.java
@@ -0,0 +1,66 @@
+package tn.esprit.devops_project;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.devops_project.entities.Invoice;
+import tn.esprit.devops_project.entities.Operator;
+import tn.esprit.devops_project.entities.Supplier;
+import tn.esprit.devops_project.entities.SupplierCategory;
+import tn.esprit.devops_project.repositories.InvoiceRepository;
+import tn.esprit.devops_project.repositories.OperatorRepository;
+import tn.esprit.devops_project.repositories.SupplierRepository;
+import tn.esprit.devops_project.services.InvoiceServiceImpl;
+import tn.esprit.devops_project.services.SupplierServiceImpl;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class InvoiceServiceImplTest {
+
+    @InjectMocks
+    private InvoiceServiceImpl invoiceService;
+
+    @Mock
+    private InvoiceRepository invoiceRepository;
+
+    @Mock
+    private OperatorRepository operatorRepository;
+
+    @Mock
+    private SupplierRepository supplierRepository;
+        @Autowired
+        SupplierServiceImpl supplierServiceImpl;
+       @Autowired
+        InvoiceServiceImpl invoiceServiceImpl;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testRetrieveAllInvoices() {
+        List<Invoice> invoices = new ArrayList<>();
+        when(invoiceRepository.findAll()).thenReturn(invoices);
+
+        List<Invoice> result = invoiceService.retrieveAllInvoices();
+
+        assertEquals(invoices, result);
+    }
+
+
+ 
+}
